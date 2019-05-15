dfa83bd9940dcdbea46e56e314a45394f1e11108
Caleb Conner <caleb@caleb-conner.com>
2019-05-14 21:36:52 -0500

    Add hello world using command line arguments.

---
+++ hello-world/command-line-args/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace command_line_args
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+              Console.WriteLine($"Hello, {args[0]}!");
+            }
+        }
+    }
+}
